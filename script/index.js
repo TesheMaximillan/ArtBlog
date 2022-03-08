ad77f143dea84b8f48627b5cccf17579becfd5de
Teshome Kurabachew <dear.teshe@gmail.com>
2022-03-09 01:16:09 +0300

    Add header section

--- script/index.js
+++ script/index.js
@@ -1,16 +1,47 @@
+const header = document.querySelector('.header');
+const headerBody = document.querySelector('.header-body');
 const hambergerIcon = document.querySelector('#hamberger');
 const iconMenu = document.querySelector('.icon-menu');
 const iconCross = document.querySelector('.icon-cross');
-const navMobile = document.querySelector('.nav-mobile');
+
+// //////////////////// Sticky NavBar
+const navbar = document.querySelector('.header-nav');
+const sticky = navbar.offsetTop;
+
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+    headerBody.classList.add('content');
+  } else {
+    navbar.classList.remove('sticky');
+    headerBody.classList.remove('content');
+  }
+};
+
+// /////////////////// Mobile Menu
+const ul = document.createElement('ul');
+ul.classList.add('header-nav-container__nav-bar--link', 'nav-mobile');
+const navItems = ['About', 'Program', 'Join', 'Sponsor', 'News'];
+
+for (let i = 0; i < navItems.length; i += 1) {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.textContent = navItems[i];
+  li.className = 'nav-link';
+  li.appendChild(a);
+  ul.appendChild(li);
+}
+
+header.insertBefore(ul, headerBody);
 
 hambergerIcon.addEventListener('click', () => {
   if (iconMenu.classList.contains('hide-icon')) {
     iconMenu.classList.remove('hide-icon');
     iconCross.style.display = 'none';
-    navMobile.style.display = 'none';
+    ul.style.display = 'none';
   } else {
     iconMenu.classList.add('hide-icon');
     iconCross.style.display = 'inline-block';
-    navMobile.style.display = 'flex';
+    ul.style.display = 'flex';
   }
 });
