fb810cef03d45c8fb998b56ec42dda512cef2868
Teshome Kurabachew <dear.teshe@gmail.com>
2022-03-10 04:16:03 +0300

    Add featured speaker section

--- script/index.js
+++ script/index.js
@@ -5,6 +5,7 @@ const iconMenu = document.querySelector('#icon-menu');
 const iconCross = document.querySelector('#icon-cross');
 const artCardContainer = document.querySelector('.main-container__art');
 const showMore = document.querySelector('.main-container__detail');
+const speakersContainer = document.querySelector('.featured-speakers');
 
 // //////////////////// Sticky NavBar
 const navbar = document.querySelector('.header-nav');
@@ -53,49 +54,49 @@ const arts = [
   {
     img: '../assets/images/pexels-dan-cristian-pădureț-1193743-min.jpg',
     alt: 'Dan Cristian img',
-    author: 'Dan Cristian',
+    artist: 'Dan Cristian',
     site: 'https://www.pexels.com/',
   },
   {
     img: '../assets/images/pexels-dazzle-jam-1038041-min.jpg',
-    alt: '',
-    author: 'Dazzel Jam',
+    alt: 'Dazzel Jam img',
+    artist: 'Dazzel Jam',
     site: 'https://www.pexels.com/',
   },
   {
     img: '../assets/images/pexels-min-an-962312-min.jpg',
     alt: 'Min An img',
-    author: 'Min An',
+    artist: 'Min An',
     site: 'https://www.pexels.com/',
   },
   {
     img: '../assets/images/pexels-pixabay-159862-min.jpg',
     alt: 'Pixabay img',
-    author: 'Pixabay',
+    artist: 'Pixabay',
     site: 'https://www.pexels.com/',
   },
   {
     img: '../assets/images/pexels-pixabay-161154-min.jpg',
     alt: 'Pixabay img 2',
-    author: 'Pixabay',
+    artist: 'Pixabay',
     site: 'https://www.pexels.com/',
   },
   {
     img: '../assets/images/pexels-pixabay-73873-min.jpg',
     alt: 'Pixabay img 3',
-    author: 'Pixabay',
+    artist: 'Pixabay',
     site: 'https://www.pexels.com/',
   },
   {
     img: '../assets/images/pexels-sharon-mccutcheon-1209843-min.jpg',
-    author: 'Sharon Mccutcheon',
+    artist: 'Sharon Mccutcheon',
     site: 'https://www.pexels.com/',
     alt: 'Sharon Mccutcheon img',
   },
   {
     img: '../assets/images/pexels-una-laurencic-20967-min.jpg',
     alt: 'Una Laurencic img',
-    author: 'Una Laurencic',
+    artist: 'Una Laurencic',
     site: 'https://www.pexels.com/',
   },
 ];
@@ -104,22 +105,22 @@ for (let i = 0; i < arts.length; i += 1) {
   const artCard = document.createElement('div');
   const cardImg = document.createElement('img');
   const cardDesc = document.createElement('div');
-  const author = document.createElement('p');
+  const artist = document.createElement('p');
   const site = document.createElement('small');
 
   artCard.className = 'main-container__art--card';
   cardImg.className = 'card-img';
   cardDesc.className = 'card-desc';
-  author.className = 'card-desc__author';
+  artist.className = 'card-desc__artist';
   site.className = 'card-desc__site';
 
   cardImg.src = arts[i].img;
   cardImg.alt = arts[i].alt;
-  author.textContent = arts[i].author;
+  artist.textContent = arts[i].artist;
   site.textContent = arts[i].site;
 
   artCard.appendChild(cardImg);
-  cardDesc.appendChild(author);
+  cardDesc.appendChild(artist);
   cardDesc.appendChild(site);
   artCard.appendChild(cardDesc);
   artCardContainer.appendChild(artCard);
@@ -138,3 +139,96 @@ showMore.addEventListener('click', (event) => {
     showMore.textContent = 'Collapse';
   }
 });
+
+// /////////////////// Featured Speakers
+const featuredSpeakers = [
+  {
+    picture: '../assets/images/carton woman.jpg',
+    name: 'Julia Leda',
+    alt: 'Julia Leda img 1',
+    title: 'Artist who is participants of the pexel site',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laboriosam ea, error perferendis officia culpa beatae dolore porro magni in voluptatem at consequuntur dolorem sed facilis commodi ratione voluptatum iusto.',
+  },
+  {
+    picture: '../assets/images/carton man.jpg',
+    name: 'Dan Cristian',
+    alt: 'Dan Cristian img 1',
+    title: 'Artist who is participants of the pexel site',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laboriosam ea, error perferendis officia culpa beatae dolore porro magni in voluptatem at consequuntur dolorem sed facilis commodi ratione voluptatum iusto.',
+  },
+  {
+    picture: '../assets/images/carton man.jpg',
+    name: 'Dan Cristian',
+    alt: 'Dan Cristian img 2',
+    title: 'Artist who is participants of the pexel site',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laboriosam ea, error perferendis officia culpa beatae dolore porro magni in voluptatem at consequuntur dolorem sed facilis commodi ratione voluptatum iusto.',
+  },
+  {
+    picture: '../assets/images/carton woman.jpg',
+    name: 'Julia Leda',
+    alt: 'Julia Leda img 2',
+    title: 'Artist who is participants of the pexel site',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laboriosam ea, error perferendis officia culpa beatae dolore porro magni in voluptatem at consequuntur dolorem sed facilis commodi ratione voluptatum iusto.',
+  },
+  {
+    picture: '../assets/images/carton woman.jpg',
+    name: 'Julia Leda',
+    alt: 'Julia Leda img 3',
+    title: 'Artist who is participants of the pexel site',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laboriosam ea, error perferendis officia culpa beatae dolore porro magni in voluptatem at consequuntur dolorem sed facilis commodi ratione voluptatum iusto.',
+  },
+  {
+    picture: '../assets/images/carton man.jpg',
+    name: 'Dan Cristian',
+    alt: 'Dan Cristian img 3',
+    title: 'Artist who is participants of the pexel site',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laboriosam ea, error perferendis officia culpa beatae dolore porro magni in voluptatem at consequuntur dolorem sed facilis commodi ratione voluptatum iusto.',
+  },
+];
+
+const speakers = document.createElement('div');
+const speakersTitle = document.createElement('h2');
+const artists = document.createElement('div');
+
+speakers.className = 'speakers';
+speakersTitle.className = 'speakers__main-title';
+speakersTitle.textContent = 'Featured Speakers';
+artists.className = 'speakers__main-artists';
+
+speakers.appendChild(speakersTitle);
+
+for (let i = 0; i < featuredSpeakers.length; i += 1) {
+  const artist = document.createElement('div');
+  const artistPicture = document.createElement('img');
+  const aboutArtist = document.createElement('div');
+  const artistName = document.createElement('p');
+  const aritstTitle = document.createElement('p');
+  const borderLine = document.createElement('div');
+  const artistDesc = document.createElement('p');
+
+  artist.className = 'speakers__main-artists--artist';
+  artist.id = 'artist';
+  artistPicture.className = 'artist-picture';
+  aboutArtist.className = 'about-artist';
+  artistName.className = 'artist-name';
+  aritstTitle.className = 'artist-title';
+  borderLine.className = 'line';
+  artistDesc.className = 'artist-desc';
+
+  artistPicture.src = featuredSpeakers[i].picture;
+  artistPicture.alt = featuredSpeakers[i].alt;
+  artistName.textContent = featuredSpeakers[i].name;
+  aritstTitle.innerHTML = featuredSpeakers[i].title;
+  artistDesc.innerHTML = featuredSpeakers[i].content;
+
+  artist.appendChild(artistPicture);
+  aboutArtist.appendChild(artistName);
+  aboutArtist.appendChild(aritstTitle);
+  aboutArtist.appendChild(borderLine);
+  aboutArtist.appendChild(artistDesc);
+  artist.appendChild(aboutArtist);
+  artists.appendChild(artist);
+}
+
+speakers.appendChild(artists);
+speakersContainer.appendChild(speakers);
