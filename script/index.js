22726f2f5e56b7bc6d92e309366a29e6076202cc
Teshome Kurabachew <51437483+TesheMaximillan@users.noreply.github.com>
2022-03-11 03:02:10 +0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -27,23 +27,23 @@ ul.classList.add('header-nav-container__nav-bar--link', 'nav-mobile');
 const navItems = [
   {
     name: 'About',
-    href: '../pages/about.html',
+    href: './pages/about.html',
   },
   {
     name: 'Program',
-    href: '../pages/about.html',
+    href: './pages/about.html',
   },
   {
     name: 'Join',
-    href: '.../pages/about.html',
+    href: './pages/about.html',
   },
   {
     name: 'Sponsor',
-    href: '../pages/about.html',
+    href: './pages/about.html',
   },
   {
     name: 'News',
-    href: '../pages/about.html',
+    href: './pages/about.html',
   },
 ];
 
